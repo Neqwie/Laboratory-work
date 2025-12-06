e75d375d4d3532a614f5a4c2cd19306b75ac1d64
Neqwie <87112417+Neqwie@users.noreply.github.com>
2025-12-06 18:04:51 +0300

    Update main.c, made checking input

--- lab5/main.c
+++ lab5/main.c
@@ -19,7 +19,11 @@ int main() {
     printf("5 - Поиск по имени и профессии\n");
     printf("6 - Сохранение БД в файл\n");
     printf("7 - Загрузка из файла\n");
-    scanf("%i", &k);
+    if (scanf("%i", &k) != 1) {
+        printf("!      Input_Error      !\n");
+        printf("Brake up");
+        while (getchar() != '\n');
+    }
     while (k != 0) {
         switch (k) {
         case 0:
@@ -31,7 +35,11 @@ int main() {
             char name[32];
             char work[32];
             int salary;
-            scanf("%i%s%s%i", &id, name, work, &salary);
+            if (scanf("%i%s%s%i", &id, name, work, &salary) != 4) {
+                printf("!      Input_Error      !\n");
+                while (getchar() != '\n');
+                break;
+            }
             Emp e;
             e.id = id;
             strcpy(e.name, name);
@@ -45,42 +53,62 @@ int main() {
             break;
         }
         case 3: {
-            printf("Введите в следующем порядке: index(int), id(int), name(string), work(string), salary(int), если не хотите изменять типы int пишите -1, если типы string пишите 0\n");
+            printf("Введите в следующем порядке: index(int), id(int), name(string), work(string), salary(int), если не хотите изменять типы int пишите -1, если же типы string пишите 0\n");
             int id;
             int index;
             char name[32];
             char work[32];
             int salary;
-            scanf("%i%i%s%s%i", &index, &id, name, work, &salary);
+            if (scanf("%i%i%s%s%i", &index, &id, name, work, &salary) != 5) {
+                printf("!      Input_Error      !\n");
+                while (getchar() != '\n');
+                break;
+            }
             edit_element(&BD, index, id, name, work, salary);
             break;
         }
         case 4: {
             printf("Введите индекс элемента, который хотите удалить\n");
             int index;
-            scanf("%i", &index);
+            if (scanf("%i", &index) != 1) {
+                printf("!      Input_Error      !\n");
+                while (getchar() != '\n');
+                break;
+            }
             v_remove(&BD, index);
             break;
         }
         case 5: {
-            printf("Введите в начале имя, а потом и название профессии работника\n");
+            printf("Введите в начале имя, a потом и название профессии работника\n");
             char name[32];
             char work[32];
-            scanf("%s%s", name, work);
+            if (scanf("%s%s", name, work) != 2) {
+                printf("!      Input_Error      !\n");
+                while (getchar() != '\n');
+                break;
+            }
             find_name_work(&BD, name, work);
             break;
         }
         case 6: {
             printf("Введите название файла(.bin), куда хотите сохранить БД\n");
             char str[64];
-            scanf("%s", str);
+            if (scanf("%s", str) != 1) {
+                printf("!      Input_Error      !\n");
+                while (getchar() != '\n');
+                break;
+            }
             save_in_file(&BD, str);
             break;
         }
         case 7: {
             printf("Введите название файла(.bin), откуда хотите считать данные в БД\n");
             char str[64];
-            scanf("%s", str);
+            if (scanf("%s", str) != 1) {
+                printf("!      Input_Error      !\n");
+                while (getchar() != '\n');
+                break;
+            }
             read_in_file(&BD, str);
             }
         }
@@ -92,7 +120,11 @@ int main() {
         printf("5 - Поиск по имени и профессии\n");
         printf("6 - Сохранение БД в файл\n");
         printf("7 - Загрузка из файла\n");
-        scanf("%i", &k);
+        if (scanf("%i", &k) != 1) {
+            printf("!      Input_Error      !\n");
+            while (getchar() != '\n');
+            break;
+        }
     }
     free_v(&BD);
     printf("End of the program");
